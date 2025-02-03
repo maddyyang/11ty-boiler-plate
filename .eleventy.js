2628630e3e1cd99d20afed7cb97b07d2136367a8
maddyyang <131908833+maddyyang@users.noreply.github.com>
2025-02-03 04:55:26 +0000

    styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
-
 module.exports = function(eleventyConfig) {
   // Create a "posts" collection from Markdown files in the posts folder
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("posts/*.md");
   });
+
+  // Tell Eleventy to copy the "styles" folder to "_site"
+  eleventyConfig.addPassthroughCopy("styles");
 
   return {
     dir: {
@@ -13,3 +15,4 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
+
